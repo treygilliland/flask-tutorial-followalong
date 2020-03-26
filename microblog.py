8ad3ed99340f1eb7c321a78dab4be3658e651273
Trey Gilliland <jlg2266@columbia.edu>
2020-03-25 23:08:55 -0400

    Initial commit, up to Chp. 9

---
+++ microblog.py
@@ -0,0 +1,7 @@
+# 1st is module, 2nd Flask instance
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
